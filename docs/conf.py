50b820a0f2d2a619bc52a74c28f725b522856d65
Dimitris Tsapetis <dimtsap@hotmail.com>
2021-02-01 16:58:21 -0500

    cccc

--- docs/conf.py
+++ docs/conf.py
@@ -161,7 +161,7 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx refer to the Python standard library.
-intersphinx_mapping = {
-    'python' ('httpsdocs.python.org3', None),
-    'numpy' ('httpsdocs.scipy.orgdocnumpy', None),
-}
+# intersphinx_mapping = {
+#     'python' ('httpsdocs.python.org3', None),
+#     'numpy' ('httpsdocs.scipy.orgdocnumpy', None),
+# }
